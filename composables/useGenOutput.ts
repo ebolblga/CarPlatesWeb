5f84db6c166b398b8fa6cc8ba653af9b5f53c45f
ahibis <nikitonvg@list.ru>
2022-08-22 21:19:19 +0300

    flex

--- composables/useGenOutput.ts
+++ composables/useGenOutput.ts
@@ -1,25 +1,25 @@
 import { type } from "os";
 
-async function GenLetterOutput(url = "russianUTF-8.txt") {
-  const data: string = await $fetch("/Library/" + url);
-  console.log(data);
-
-  let words = data.split("\n");
-  if (words.length && words[0].at(-1) == "\r")
-    words = words.map((word) => word.slice(0, -1));
-
+async function GenLetterOutput(data = "") {
+  let words = data
+    .split("\n")
+    .map((word) => (word.at(-1) == "\r" ? word.slice(0, -1) : word));
   // Доступные буквы: ABЕКМНОРСТУХ
   // Доступные цифры: 03469 (озчбд)
   const ans1 = words
-    .filter((word) =>
-      /^[авекмнорстух]?[озчбд]{3}[авекмнорстух]{0,2}$/i.test(word)
+    .filter(
+      (word) =>
+        /^[авекмнорстух]?[озчбд]{3}[авекмнорстух]{0,2}$/i.test(word) &&
+        word.length
     )
     .map((word) =>
       /(^[авекмнорстух]?)([озчбд]{3})([авекмнорстух]{0,2})$/i.exec(word)
     )
     .map(([a, b, c, d]) => b.padStart(1, "*") + c + d.padEnd(2, "*"));
   const ans2 = words
-    .filter((word) => /^[авекмнорстух]?[озчбд]{0,2}$/i.test(word))
+    .filter(
+      (word) => /^[авекмнорстух]?[озчбд]{0,2}$/i.test(word) && word.length
+    )
     .map((word) => /^([авекмнорстух]?)([озчбд]{0,2})$/i.exec(word))
     .map(([a, b, c]) => b.padStart(1, "*") + c.padEnd(5, "*"));
   const ans3 = words
@@ -29,47 +29,107 @@ async function GenLetterOutput(url = "russianUTF-8.txt") {
   return [...ans1, ...ans2, ...ans3];
 }
 
-async function Gen3LetterOutput(url = "russianUTF-8.txt") {
-  const data: string = await $fetch("/Library/" + url);
-
-  let words = data.split("\n");
-  if (words.length && words[0].at(-1) == "\r")
-    words = words.map((word) => word.slice(0, -1));
-
+async function Gen3LetterOutput(data = "") {
+  let words = data
+    .split("\n")
+    .map((word) => (word.at(-1) == "\r" ? word.slice(0, -1) : word));
   return words.filter((word) => /^[авекмнорстух]{3}$/i.test(word));
 }
-async function Gen6LetterOutput(url = "russianUTF-8.txt") {
-  const data: string = await $fetch("/Library/" + url);
-
-  let words = data.split("\n");
-  if (words.length && words[0].at(-1) == "\r")
-    words = words.map((word) => word.slice(0, -1));
+async function Gen6LetterOutput(data = "") {
+  let words = data
+    .split("\n")
+    .map((word) => (word.at(-1) == "\r" ? word.slice(0, -1) : word));
 
   return words.filter((word) =>
     /^[авекмнорстух][озчбд]{3}[авекмнорстух]{2}$/i.test(word)
   );
 }
+async function GenLetterOutputUrl(url = "russianUTF-8.txt") {
+  const data: string = await $fetch("/Library/" + url);
+  return GenLetterOutput(data);
+}
+async function Gen3LetterOutputUrl(url = "russianUTF-8.txt") {
+  const data: string = await $fetch("/Library/" + url);
+  return Gen3LetterOutput(data);
+}
+async function Gen6LetterOutputUrl(url = "russianUTF-8.txt") {
+  const data: string = await $fetch("/Library/" + url);
+  return Gen6LetterOutput(data);
+}
+async function GenLetterOutputFile(file: File) {
+  const data: string = await file.text();
+  return GenLetterOutput(data);
+}
+async function Gen3LetterOutputFile(file: File) {
+  const data: string = await file.text();
+  return Gen3LetterOutput(data);
+}
+async function Gen6LetterOutputFile(file: File) {
+  const data: string = await file.text();
+  return Gen6LetterOutput(data);
+}
 type Type = "" | "6" | "3";
-export function useGenOutput(url = "russianUTF-8.txt", type: Type = "") {
-  const loading = ref(true);
-  const error = ref(false);
-  const data = ref([]);
+
+export function useGenOutputFile(
+  file: File,
+  type: Type = "",
+  callback = (e) => {}
+) {
+  const response = reactive({
+    loading: true,
+    error: false,
+    data: [],
+  });
   const funcs = {
-    "": GenLetterOutput,
-    "3": Gen3LetterOutput,
-    "6": Gen6LetterOutput,
+    "": GenLetterOutputFile,
+    "3": Gen3LetterOutputFile,
+    "6": Gen6LetterOutputFile,
+  };
+
+  funcs[type](file)
+    .then((Data) => {
+      console.log(Data);
+      response.data = Data;
+      response.loading = false;
+      callback(Data);
+    })
+    .catch((e) => {
+      response.error = true;
+      response.loading = false;
+      console.error(e);
+    });
+
+  return response;
+}
+
+export function useGenOutput(
+  url = "russianUTF-8.txt",
+  type: Type = "",
+  callback = (e) => {}
+) {
+  const response = reactive({
+    loading: true,
+    error: false,
+    data: [],
+  });
+  const funcs = {
+    "": GenLetterOutputUrl,
+    "3": Gen3LetterOutputUrl,
+    "6": Gen6LetterOutputUrl,
   };
 
   funcs[type](url)
     .then((Data) => {
       console.log(Data);
-      data.value = Data;
-      loading.value = false;
+      response.data = Data;
+      response.loading = false;
+      callback(Data);
     })
     .catch((e) => {
-      error.value = true;
-      loading.value = false;
+      response.error = true;
+      response.loading = false;
+      console.error(e);
     });
 
-  return { loading, error, data };
+  return response;
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,59 @@
 <script setup>
 useHead({ title: "Генератор" });
-
+const files = {
+  "russianUTF-8.txt": "все русские слова",
+  "ebeba.txt": "смешные слова",
+  "swear.txt": "бранные слова",
+  "": "свой файл"
+}
+const file = ref("russianUTF-8.txt")
+const types = {
+  "": "все буквы",
+  "3": "3 буквенные",
+  "6": "6 буквенные"
+}
+const type = ref("")
+const request = ref({
+  data: [],
+  loading: false,
+  error: false
+})
 function Search() {
-  let { data, loading, error } = useGenOutput("ebeba.txt", "6");
+  if (file.value == "") {
+    const input = document.createElement("input")
+    input.type = "file"
+    input.accept = ".txt"
+    input.click()
+    input.onchange = e => {
+      const file = input.files[0]
+      request.value = useGenOutputFile(file, type.value)
+    }
+  } else {
+    request.value = useGenOutput(file.value, type.value);
+  }
+
 }
 </script>
 
 <template>
   <div class="content-center text-center pt-[10%]">
-    <DropdownRadio />
-    <button
-      type="button"
-      @click="Search"
-      class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium focus:outline-none rounded-lg border focus:z-10 focus:ring-4 focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700"
-    >
+    <!-- <DropdownRadio /> -->
+    <select v-model="file">
+      <option v-for="file, i in files" :value="i">{{ file }}</option>
+    </select><br />
+    <select v-model="type">
+      <option v-for="type, i in types" :value="i">{{ type }}</option>
+    </select>
+    <br />
+    <button type="button" @click="Search"
+      class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium focus:outline-none rounded-lg border focus:z-10 focus:ring-4 focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700">
       Поиск
-    </button>
-    {{ loading }} {{ error }} {{ data }}
+    </button><br />
+    <span v-if="request.loading">
+      загрузка...
+    </span>
+    <span v-else>
+      {{ request.error }} {{ request.data }}
+    </span>
   </div>
 </template>
