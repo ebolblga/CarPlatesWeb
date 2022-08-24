71f1c7751a5b7503f6766782e3ef7090c38893bc
ebolblga <kkochanovskiy@gmail.com>
2022-08-24 21:45:50 +0300

    Broke /test

--- pages/test.vue
+++ pages/test.vue
@@ -1,18 +1,23 @@
-<script setup lang="ts">
+<script setup>
 useHead({ title: "Тестирование" });
 let isShown = ref(false);
 
-// let img = new Image();
-// function drawImg(){
-//     img.src = '/TemplateRU.png';
-//     img.font = "12px";
-//     img.fillText("B639YH", 2, 2);
-// }
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+ctx.src = "/TemplateRU.png";
+ctx.font = "12px Arial";
+ctx.fillText("B639YH", 2, 2);
 </script>
 
 <template>
   <div class="content-center text-center pt-[5vh]">
     <my-button @click="isShown = !isShown">Сгенерировать</my-button>
-    <nuxt-img src="/TemplateRU.png" class="w-[156px] mx-auto pt-5" :class="{hidden: !isShown}" alt="Plate" />
+    <nuxt-img
+      src="/TemplateRU.png"
+      class="w-[156px] mx-auto pt-5"
+      :class="{ hidden: !isShown }"
+      alt="Plate"
+    />
+    <canvas height="36" width="156" id="canvas">Обновите браузер</canvas>
   </div>
 </template>
