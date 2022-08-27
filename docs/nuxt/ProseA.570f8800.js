2161ceabea19a6a1ed21b1604ecc421ac7542e37
ahibis <nikitonvg@list.ru>
2022-08-27 21:26:09 +0300

    test

---
+++ docs/nuxt/ProseA.570f8800.js
@@ -0,0 +1,1 @@
+import{a,o as n,c as r,p as s,r as f,q as l}from"./entry.69c5baba.js";const c=a({__name:"ProseA",props:{href:{type:String,default:""},blank:{type:Boolean,default:!1}},setup(e){return(t,p)=>{const o=l;return n(),r(o,{href:e.href},{default:s(()=>[f(t.$slots,"default")]),_:3},8,["href"])}}});export{c as default};
---
+++ docs/nuxt/ProseCodeInline.37ef6948.js
@@ -0,0 +1,1 @@
+import{s as o,o as r,v as n,r as s}from"./entry.69c5baba.js";const t={};function c(e,a){return r(),n("code",null,[s(e.$slots,"default")])}const d=o(t,[["render",c]]);export{d as default};
